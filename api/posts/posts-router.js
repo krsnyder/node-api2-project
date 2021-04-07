5869b38188b3ccf0b9e19b15190cb98753c2eda4
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-06 21:16:23 -0400

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -60,10 +60,36 @@ router.post('/', (req, res) => {
   }
 })
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   const { id } = req.params;
+  const changes = req.body;
 
+  try {
+    const targetPost = await Post.findById(id)
+    if (!targetPost) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist"
+      })
+    } else {
+      if (!changes.title || !changes.contents) {
+        res.status(400).json({
+          message: "Please provide title and contents for the post"
+        })
+      } else {
+        Post.update(id, changes)
+          .then(updatedId => {
+            return(Post.findById(updatedId))
+          })
+          .then(updatedPost => {
+            res.status(200).json(updatedPost)
+          })
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The post information could not be modified"
+    })
+  }
 })
-
 
 module.exports = router
